b3bef5b31a7f9edc77c7f4a3ad981246495bb2ee
Dmytro Shynkevych <dm.shynk@gmail.com>
2020-04-25 10:20:11 -0500

    Add views to flatten relationships

--- hasura/migrations/1587798396156_init/up.sql
+++ hasura/migrations/1587798396156_init/up.sql
@@ -23,6 +23,11 @@ CREATE TABLE user_friend (
   PRIMARY KEY (from_id, to_id)
 );
 
+CREATE VIEW user_friend_view AS
+SELECT uf.from_id as user_id, u.*
+  FROM user_friend uf LEFT JOIN "user" u
+    ON uf.to_id = u.id;
+
 CREATE TABLE community (
   id INT PRIMARY KEY
     GENERATED ALWAYS AS IDENTITY,
@@ -42,6 +47,16 @@ CREATE TABLE community_user (
   PRIMARY KEY (community_id, user_id)
 );
 
+CREATE VIEW community_user_view AS
+SELECT cu.community_id, u.*
+  FROM community_user cu LEFT JOIN "user" u
+    ON cu.user_id = u.id;
+
+CREATE VIEW user_community_view AS
+SELECT cu.user_id, c.*
+  FROM community_user cu LEFT JOIN community c
+    ON cu.community_id = c.id;
+
 CREATE TABLE community_child (
   community_id INT NOT NULL
     REFERENCES community(id)
@@ -54,6 +69,10 @@ CREATE TABLE community_child (
   PRIMARY KEY (community_id, child_id)
 );
 
+CREATE VIEW community_child_view AS
+SELECT cc.community_id, c.*
+  FROM community_child cc LEFT JOIN community c
+    ON cc.child_id = c.id;
 
 CREATE TABLE sync (
   id INT PRIMARY KEY
@@ -89,6 +108,16 @@ CREATE TABLE sync_invited_user (
   PRIMARY KEY (sync_id, user_id)
 );
 
+CREATE VIEW sync_invited_user_view AS
+SELECT siu.sync_id, u.*
+  FROM sync_invited_user siu LEFT JOIN "user" u
+    ON siu.user_id = u.id;
+
+CREATE VIEW user_invited_sync_view AS
+SELECT siu.user_id, s.*
+  FROM sync_invited_user siu LEFT JOIN sync s
+    ON siu.sync_id = s.id;
+
 CREATE TABLE checkpoint (
   id INT PRIMARY KEY
     GENERATED ALWAYS AS IDENTITY,
@@ -111,3 +140,13 @@ CREATE TABLE user_completed_checkpoint (
     ON UPDATE CASCADE,
   PRIMARY KEY (checkpoint_id, user_id)
 );
+
+CREATE VIEW user_completed_checkpoint_view AS
+SELECT ucc.checkpoint_id, u.*
+  FROM user_completed_checkpoint ucc LEFT JOIN "user" u
+    ON ucc.user_id = u.id;
+
+CREATE VIEW checkpoint_completed_by_user_view AS
+SELECT ucc.user_id, c.*
+  FROM user_completed_checkpoint ucc LEFT JOIN checkpoint c
+    ON ucc.checkpoint_id = c.id;
