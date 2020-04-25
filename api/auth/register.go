332c676c7e5ce975a8839a53ed99d194beea5755
Dmytro Shynkevych <dm.shynk@gmail.com>
2020-04-25 15:01:42 -0400

    Fix API register endpoint to conform to Hasura action

--- api/auth/register.go
+++ api/auth/register.go
@@ -12,14 +12,16 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type registerRequest struct {
+type registerCredentials struct {
 	Email    string `json:"email"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 type registerPayload struct {
-	Credentials registerRequest `json:"credentials"`
+	Input struct {
+		Credentials registerCredentials `json:"credentials"`
+	} `json:"input"`
 }
 
 type registerResponse struct {
@@ -33,14 +35,14 @@ INSERT INTO "user"(email, password_hash, username)
 VALUES ($1, $2, $3) RETURNING id
 `
 
-func register(tx *db.Tx, request *registerRequest) (*registerResponse, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcryptCost)
+func register(tx *db.Tx, credentials *registerCredentials) (*registerResponse, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(credentials.Password), bcryptCost)
 	if err != nil {
 		return nil, fmt.Errorf("hashing password: %w", err)
 	}
 
 	var userId int
-	err = tx.QueryRow(insertUserQuery, request.Email, hash, request.Username).Scan(&userId)
+	err = tx.QueryRow(insertUserQuery, credentials.Email, hash, credentials.Username).Scan(&userId)
 	if err != nil {
 		return nil, handle.WithEnum(handle.EmailTaken, fmt.Errorf("inserting user: %w", err))
 	}
@@ -65,7 +67,7 @@ func HandleRegister(tx *db.Tx, r *http.Request) (interface{}, error) {
 		return nil, fmt.Errorf("malformed JSON: %w", err)
 	}
 
-	credentials := &payload.Credentials
+	credentials := &payload.Input.Credentials
 	if credentials.Email == "" || credentials.Password == "" || credentials.Username == "" {
 		return nil, fmt.Errorf("empty username, email, or password")
 	}
