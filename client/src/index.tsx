bd2422f31e8996947d036d867620feb4d28e158b
Harry Tong <harry.tong@uwaterloo.ca>
2020-04-26 01:34:15 -0400

    maybe got audio chat working

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ThemeProvider } from 'styled-components';
+import { Provider as ReduxProvider } from 'react-redux';
+import * as SWRTC from '@andyet/simplewebrtc';
 
 import apolloClient from './graphql/Apollo';
 import App from './App';
@@ -10,11 +12,15 @@ import Theme from './constants/Theme';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const store = SWRTC.createStore();
+
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
       <ApolloProvider client={apolloClient}>
-        <App />
+        <ReduxProvider store={store}>
+          <App />
+        </ReduxProvider>
       </ApolloProvider>
     </ThemeProvider>
   </React.StrictMode>,
