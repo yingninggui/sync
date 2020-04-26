2c487ee9630555f7983d68f43ee66a80a0e83927
Harry Tong <harry.tong@uwaterloo.ca>
2020-04-26 01:28:41 -0400

    Fix spinner

--- client/src/components/common/Spinner.tsx
+++ client/src/components/common/Spinner.tsx
@@ -1,3 +1,9 @@
-import Spinner from 'reactstrap';
+import React from 'react';
+import { Spinner as ReactstrapSpinner } from 'reactstrap';
+import { withTheme } from 'styled-components';
 
-export default Spinner;
+const Spinner: React.FC<any> = ({ theme }) => (
+  <ReactstrapSpinner style={{ color: theme.primary }} />
+);
+
+export default withTheme(Spinner);
